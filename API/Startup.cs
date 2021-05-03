c4b240f7bfe3527490033487cc0a43f9d2b9627e
ShayanVaghei <shayan@vaghei.net>
2021-05-03 14:46:02 -0400

    Adding client side to the application

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,8 @@ namespace API
                 optiions.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,7 +55,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
